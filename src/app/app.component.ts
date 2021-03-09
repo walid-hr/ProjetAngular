05a66cc1a5f2555bc15fb3edcdcadb2c7dbeca91
walid-hr <hriziwalid5@gmail.com>
2021-03-09 21:33:15 +0100

    newDirectives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'EpiTp';
   name: string="";
   Items=[
-    { name: 'walid hrizi', age: 27 ,  imgurl:"https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.0-1/c0.17.320.320a/p320x320/48414437_2055836847828770_1104734299418001408_o.jpg?_nc_cat=107&ccb=3&_nc_sid=7206a8&_nc_ohc=lXmPKvrvSyMAX-P4gfy&_nc_ht=scontent.fnbe1-2.fna&tp=27&oh=1961b9f871110d1e3ba5f58f8ba7fee9&oe=60627F71"
+    { name: 'walid hrizi', age: 26 ,  imgurl:"https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.0-1/c0.17.320.320a/p320x320/48414437_2055836847828770_1104734299418001408_o.jpg?_nc_cat=107&ccb=3&_nc_sid=7206a8&_nc_ohc=lXmPKvrvSyMAX-P4gfy&_nc_ht=scontent.fnbe1-2.fna&tp=27&oh=1961b9f871110d1e3ba5f58f8ba7fee9&oe=60627F71"
   },
     { name: 'hrizi ', age: 27 ,  imgurl:"https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.0-1/c0.17.320.320a/p320x320/48414437_2055836847828770_1104734299418001408_o.jpg?_nc_cat=107&ccb=3&_nc_sid=7206a8&_nc_ohc=lXmPKvrvSyMAX-P4gfy&_nc_ht=scontent.fnbe1-2.fna&tp=27&oh=1961b9f871110d1e3ba5f58f8ba7fee9&oe=60627F71"
   },
-    { name: 'walid', age: 27 ,   imgurl:"https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.0-1/c0.17.320.320a/p320x320/48414437_2055836847828770_1104734299418001408_o.jpg?_nc_cat=107&ccb=3&_nc_sid=7206a8&_nc_ohc=lXmPKvrvSyMAX-P4gfy&_nc_ht=scontent.fnbe1-2.fna&tp=27&oh=1961b9f871110d1e3ba5f58f8ba7fee9&oe=60627F71"
+    { name: 'walid', age: 28 ,   imgurl:"https://scontent.fnbe1-2.fna.fbcdn.net/v/t1.0-1/c0.17.320.320a/p320x320/48414437_2055836847828770_1104734299418001408_o.jpg?_nc_cat=107&ccb=3&_nc_sid=7206a8&_nc_ohc=lXmPKvrvSyMAX-P4gfy&_nc_ht=scontent.fnbe1-2.fna&tp=27&oh=1961b9f871110d1e3ba5f58f8ba7fee9&oe=60627F71"
   },
     ];
   onClick($event:any){
